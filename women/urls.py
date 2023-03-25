b2c3705582726e676ce11bef9dfc013056158d1c
Alexander Seryakov <seryakov78116@gmail.com>
2023-03-25 19:23:18 +0300

    Added authentication and authorization by session.

--- women/urls.py
+++ women/urls.py
@@ -6,6 +6,7 @@ from rest_framework import routers
 
 urlpatterns = [
     path('', get_home, name='home'),
+    path('v1/auth-drf/', include('rest_framework.urls')),
     path('v1/women/', WomenAPIList.as_view()),
     path('v1/women/<int:pk>/', WomenAPIUpdate.as_view()),
     path('v1/womendestroy/<int:pk>/', WomenAPIDestroy.as_view()),
