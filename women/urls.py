6fb14ab3c7ddb1336a8aa1fb8cf73ae641033afd
Alexander Seryakov <seryakov78116@gmail.com>
2023-03-26 23:28:52 +0300

    Added JWT token authentication - Simple JWT lib. Update requirements.txt

--- women/urls.py
+++ women/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include, re_path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
 from .views import get_home
 from .views import WomenAPIList, WomenAPIUpdate, WomenAPIDestroy
 from rest_framework import routers
@@ -12,6 +14,8 @@ urlpatterns = [
     path('v1/womendestroy/<int:pk>/', WomenAPIDestroy.as_view()),
     path('v1/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
-
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
 ]
